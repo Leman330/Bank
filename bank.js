ce3407f695d152a5c729d1730506d9d70db5d08c
Mirzayeva <askerova.leman330@gmail.com>
2020-07-16 00:42:11 +0400

    Added new functionality

--- bank.js
+++ bank.js
@@ -1,23 +1,50 @@
 $(document).ready(function(){
-    let saygac = 0;
+    
 
+    //for desktop
+    let x = $("#one").width();
+
+    //kredit
+    let ksay = 0;
     $("#kredit").click(function(){
         $("#firstkredit").slideToggle("slow");
-        $("#kredit").text(saygac? "Kredit":"Kreditin hesablanması");
-        saygac = 1 - saygac;
+        $("#kredit").text(ksay? "Kredit":"Kreditin hesablanması");
+        ksay = 1 - ksay;
+        //$(this).css("margin","auto")
+        
+
 
     });	
+    
 
+    //depozit
     let hsay = 0;
     $("#deposit").click(function(){
         $("#firstdeposit").slideToggle("slow");
         $("#deposit").text( hsay? "Depozit":"Depozitin hesablanması");
         hsay = 1 - hsay;
     });	
+
+    
+    //debet kart
+    let zsay = 0;
+    $("#card").click(function(){
+        $("#firstcard").slideToggle("slow");
+        $("#card").text( zsay? "Debet kart":"Debet kartların sifarişi");
+        zsay = 1 - zsay;
+    });	
+
+    //valyuta
+    let ysay = 0;
+    $("#valyuta").click(function(){
+        $("#firstvalyuta").slideToggle("slow");
+        $("#valyuta").text( ysay? "Məzənnə":"Günün məzənnəsi");
+        ysay = 1 - ysay;
+    });	
+
+
 });
 
-$(document).ready(function(){
-  
-});
+$(document).ready(function(){});
 
 
